162425db39522222d145e3eeb133cecbbc04e7d5
AndriiMishch <anderito02@gmail.com>
2023-10-26 10:58:47 +0200

    Create selectors.js

---
+++ src/redux/contacts/selectors.js
@@ -0,0 +1,18 @@
+import { selectFiltersSearch } from 'redux/filtres/selectors';
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectContacts = state => state.contacts.contacts;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFiltersSearch],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
+
+export const selectIsLoading = state => state.contacts.isLoading;
+
+export const selectContactsCount = state => state.contacts.contacts.length;
