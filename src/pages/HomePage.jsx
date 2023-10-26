56863ec3fabcd33b528e281b9b05badffcaa1b25
AndriiMishch <anderito02@gmail.com>
2023-10-26 10:58:14 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,31 @@
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserIsLogged } from 'redux/auth/selectors';
+
+export default function HomePage() {
+  const navigate = useNavigate();
+  const isLogged = useSelector(selectUserIsLogged);
+
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        marginTop: 300,
+      }}
+    >
+      <h1 style={{ marginBottom: 20 }}>
+      </h1>
+      {!isLogged ? (
+        <Button variant="contained" onClick={() => navigate('/login')}>
+          Log In
+        </Button>
+        
+      ) : (
+        <Button variant="contained" onClick={() => navigate('/contacts')}>
+          Contacts
+        </Button>
+      )}
+    </div>
+  );
+}
